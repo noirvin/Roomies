1212f182114d49903a1333dd2d19caf709f9aa7a
noirvin <vin.sp97@googlemail.com>
2019-10-18 14:48:07 -0700

    final commit before presentation

--- server.js
+++ server.js
@@ -48,12 +48,12 @@ mongoose.Promise = global.Promise;
 //
 //
 // //monitor requests
-//
-// app.listen(3000, ()=>{
-//
-//     console.log("server is listening on port 3000");
-// });
 
-app.listen(process.env.PORT || 3000 (req, res) => {
-    console.log("Listening at port 3000")
-})
+ app.listen(3000, ()=>{
+
+    console.log("server is listening on port 3000");
+ });
+
+//app.listen(process.env.PORT || 3000 (req, res) => {
+//   console.log("Listening at port 3000")
+//})
